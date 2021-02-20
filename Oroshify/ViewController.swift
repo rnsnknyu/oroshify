bb8e73f1f7a5c1499ed2d619e67118b6dad0391f
rnsnknyu <fytr.0730@gmail.com>
2021-02-20 15:50:03 +0900

    Initial Commit

---
+++ Oroshify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Oroshify
+//
+//  Created by Tomonari Ishida on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
